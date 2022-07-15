4c650236f9e9470f5e0ed861e906ca566e0149be
shrutisaxenaa <106474924+shrutisaxenaa@users.noreply.github.com>
2022-07-15 00:09:00 -0500

    firstCommitLead

---
+++ src/main/java/gitPractice3/GitPractice3.java
@@ -0,0 +1,15 @@
+package gitPractice3;
+
+public class GitPractice3 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(sum(4,8));
+
+	}
+	
+	public static int sum(int a, int b) {
+		return a+b;
+	}
+
+}
